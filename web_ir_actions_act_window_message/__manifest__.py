b473f43d9cd767d7e6866cc480a3022fb8890449
Jose Zambudio Bernabeu <jose@aurestic.es>
2022-05-27 08:30:07 +0200

    [ADD] Backport web_ir_actions_act_window_message

---
+++ web_ir_actions_act_window_message/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Client side message boxes",
+    "version": "11.0.1.0.0",
+    "author": "Therp BV, " "ACSONE SA/NV, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Hidden/Dependency",
+    "summary": "Show a message box to users",
+    "depends": [
+        "web",
+    ],
+    "data": [
+        "views/templates.xml",
+    ],
+    "qweb": [
+        "static/src/xml/web_ir_actions_act_window_message.xml",
+    ],
+}
